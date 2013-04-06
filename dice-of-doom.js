da4483955266f05c4a0ea31b58ddda65131b98bc
Kana Natsuno <dev@whileimautomaton.net>
2013-04-06 20:53:34 +0900

    Define several constants

--- dice-of-doom.js
+++ dice-of-doom.js
@@ -1,3 +1,8 @@
 (function () {
+  var numberOfPlayers = 2;
+  var maximumDice = 3;
+  var boardWidth = 2;
+  var boardHeight = 2;
+  var boardHexCount = boardWidth * boardHeight;
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2
