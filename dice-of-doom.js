ac159a38af51be1fcfb138a31c8382499a0f617a
Kana Natsuno <dev@whileimautomaton.net>
2013-04-06 20:57:25 +0900

    Define random

--- dice-of-doom.js
+++ dice-of-doom.js
@@ -4,5 +4,9 @@
   var boardWidth = 2;
   var boardHeight = 2;
   var boardHexCount = boardWidth * boardHeight;
+
+  function random(n) {
+    return Math.floor(Math.random() * n);
+  }
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2
