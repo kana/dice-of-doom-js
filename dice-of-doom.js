749a347454053f220206dcae71bc45a16b8a81c6
Kana Natsuno <dev@whileimautomaton.net>
2013-04-06 21:47:03 +0900

    Define drawBoardAsText

--- dice-of-doom.js
+++ dice-of-doom.js
@@ -40,5 +40,21 @@
     }
     return board;
   }
+
+  function drawBoardAsText(board) {
+    var cs = [];
+    for (var y = 0; y < boardHeight; y++) {
+      cs.push(repeat('  ', boardHeight - (y + 1)));
+      for (var x = 0; x < boardWidth; x++) {
+        var hex = board[y * boardWidth + x];
+        cs.push('[');
+        cs.push(makePlayerName(hex.player));
+        cs.push(hex.diceCount);
+        cs.push(']');
+      }
+      cs.push('\n');
+    }
+    return cs.join('');
+  }
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2
